5a69fd0e72c9765ad2ba52900930eeed3d74098a
EvgeniyLV <zhenia.larionoff2015@yandex.ru>
2022-07-03 18:45:57 +0300

    правки для community

--- src/components/Community.vue
+++ src/components/Community.vue
@@ -4,48 +4,51 @@
     <p class="community__subtitle">Тысячи учёных уже предлагают свою помощь</p>
 
     <div class="scientists">
-      <article class="scientists-card">
+      <article
+        class="scientists-card"
+        v-for="(item, index) in items"
+        :key="index"
+      >
         <ul class="scientists-tabs">
-          <li class="scientists-tabs__item">Цитировать других</li>
-          <li class="scientists-tabs__item">Neuro/Psycho</li>
+          <li
+            v-for="(tab, index) in item.tabs"
+            :key="index"
+            class="scientists-tabs__item"
+          >
+            {{ tab.text }}
+          </li>
         </ul>
         <h3 class="scientists-card__title">
-          Вопросы проектирования трансатлантического межнационального
-          продуктопровода в условиях нарастающей коронавирусной пандемии
+          {{ item.title }}
         </h3>
         <div class="comment">
           <h4 class="comment__title">Комментарий</h4>
           <p class="comment__text">
-            Могу ещё помочь со статистической обработкой
+            {{ item.comment }}
           </p>
         </div>
         <div class="award">
           <h4 class="award__title">Вознаграждение</h4>
           <ul class="award-tabs">
-            <li class="award-tabs__item">Процитировать других</li>
-            <li class="award-tabs__item">Принять в соавторы</li>
-            <li class="award-tabs__item">+1</li>
+            <li
+              class="award-tabs__item"
+              v-for="(item, index) in item.award"
+              :key="index"
+            >
+              {{ item.text }}
+            </li>
           </ul>
         </div>
         <div class="keys-wrapper">
           <ul class="keys">
-            <li class="keys__item">
-              <img
-                class="keys__image"
-                src="../assets/images/key-image.svg"
-                alt="key-image"
-              />
-              первый длинный ключ
+            <li
+              class="keys__item"
+              v-for="(item, index) in item.keys"
+              :key="index"
+            >
+              <img class="keys__image" :src="item.image" alt="key-image" />
+              {{ item.text }}
             </li>
-            <li class="keys__item">
-              <img
-                class="keys__image"
-                src="../assets/images/key-image.svg"
-                alt="key-image"
-              />
-              второй ключ
-            </li>
-            <li class="keys__item">+1</li>
           </ul>
         </div>
         <div class="scientist">
@@ -55,15 +58,15 @@
             alt="scientist"
           />
           <div class="scientist__info">
-            <h4 class="scientist__name">Артём Белоусов</h4>
-            <p class="scientist__degree">учёный (кандидат наук)</p>
+            <h4 class="scientist__name">{{ item.name }}</h4>
+            <p class="scientist__degree">{{ item.degree }}</p>
             <p class="scientist__university">
-              Санкт-Петербургский горный университет
+              {{ item.university }}
             </p>
           </div>
         </div>
         <div class="card-footer">
-          <button class="card-footer__button-left">
+          <button class="card-footer__button-left" v-if="item.cooperation">
             <img src="../assets/images/done.svg" alt="key-image" />
             Сотрудничество предложено
           </button>
@@ -86,7 +89,7 @@ export default {
           tabs: [{ text: "Цитировать других" }, { text: "Neuro/Psycho" }],
           title:
             "Вопросы проектирования трансатлантического межнационального продуктопровода в условиях нарастающей коронавирусной пандемии",
-          comment: " Могу ещё помочь со статистической обработкой",
+          comment: "Могу ещё помочь со статистической обработкой",
           award: [
             { text: "Процитировать других" },
             { text: "Принять в соавторы" },
